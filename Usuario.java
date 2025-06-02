81599210be3b6a282afaea2d3b4652722600af41
Iasmin Cristina F. T. S. Prado <110954260+iasmincft@users.noreply.github.com>
2025-06-02 12:29:07 -0300

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,18 @@
+public abstract class Usuario {
+    protected int id;
+    protected String nome;
+
+    public Usuario(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public abstract boolean podeEmprestar();
+}
